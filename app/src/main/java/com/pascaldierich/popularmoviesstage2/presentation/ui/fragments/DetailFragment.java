c86bf2e943cbbec13cdd211d2c76548fa79f0dd0
Pascal Dierich <dierich.pascal@gmail.com>
2016-12-12 19:53:45 +0100

    add fragments and layout files

---
+++ app/src/main/java/com/pascaldierich/popularmoviesstage2/presentation/ui/fragments/DetailFragment.java
@@ -0,0 +1,29 @@
+package com.pascaldierich.popularmoviesstage2.presentation.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+import com.pascaldierich.popularmoviesstage2.presentation.ui.BaseView;
+
+/**
+ * Created by pascaldierich on 12.12.16.
+ */
+
+public class DetailFragment extends Fragment implements BaseView {
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+}
---
+++ app/src/main/java/com/pascaldierich/popularmoviesstage2/presentation/ui/fragments/MovieFragment.java
@@ -0,0 +1,29 @@
+package com.pascaldierich.popularmoviesstage2.presentation.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+import com.pascaldierich.popularmoviesstage2.presentation.ui.BaseView;
+
+/**
+ * Created by pascaldierich on 12.12.16.
+ */
+
+public class MovieFragment extends Fragment implements BaseView {
+    private static final String LOG_TAG = MovieFragment.class.getSimpleName();
+    
+    
+    @Override
+    public void showProgress() {
+        
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+}
