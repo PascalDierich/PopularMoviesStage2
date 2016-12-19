68756c6b5ece8880519bd0ef129136ba6ff7943d
Pascal Dierich <dierich.pascal@gmail.com>
2016-12-19 17:09:33 +0100

    add classes for Adapter for Trailer and Review

---
+++ app/src/main/java/com/pascaldierich/popularmoviesstage2/presentation/ui/adapter/ReviewAdapter.java
@@ -0,0 +1,8 @@
+package com.pascaldierich.popularmoviesstage2.presentation.ui.adapter;
+
+/**
+ * Created by Pascal Dierich on Dez, 2016.
+ */
+public class ReviewAdapter {
+	private static final String LOG_TAG = ReviewAdapter.class.getSimpleName();
+}
---
+++ app/src/main/java/com/pascaldierich/popularmoviesstage2/presentation/ui/adapter/TrailerAdapter.java
@@ -0,0 +1,8 @@
+package com.pascaldierich.popularmoviesstage2.presentation.ui.adapter;
+
+/**
+ * Created by Pascal Dierich on Dez, 2016.
+ */
+public class TrailerAdapter {
+	private static final String LOG_TAG = TrailerAdapter.class.getSimpleName();
+}
